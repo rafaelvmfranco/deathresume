73ae18add160cfc8872644b99bc75444f7be3c3d
Julie Cherner <juliechernen@gmail.com>
2024-02-03 15:36:23 +0200

    Server/Missing Plans module is added to subcription module

--- apps/server/src/subcription/subcription.module.ts
+++ apps/server/src/subcription/subcription.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 
 import { SubcriptionService } from "./subcription.service";
 import { FirebaseModule } from "../firebase/firebase.module";
+import { PlanModule } from "../plan/plan.module";
 
 @Module({
-  imports: [FirebaseModule],
+  imports: [FirebaseModule, PlanModule],
   providers: [SubcriptionService],
   exports: [SubcriptionService],
 })
