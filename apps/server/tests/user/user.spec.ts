d214c8cc23fa6333f85c41c860cea08b34864299
Julie Cherner <juliechernen@gmail.com>
2024-03-01 17:09:40 +0200

    Server/2 user services are added its to testing for comparison

--- apps/server/tests/user/user.spec.ts
+++ apps/server/tests/user/user.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { faker } from "@faker-js/faker";
 import { AppModule } from "../../src/app.module";
 import { UserService } from "../../src/user/user.service";
+import { FirebaseUserService } from "../../src/user/firebase-user.service";
 
 function getFakeUserCreateBody({ provider }: Record<"provider", "email" | "google">) {
   return {
@@ -19,9 +20,41 @@ function getFakeUserCreateBody({ provider }: Record<"provider", "email" | "googl
   };
 }
 
+function validateUpdate(dto: any, userResult: any, firebaseUserResult?: any){
+  expect(userResult.name).toEqual(dto.name);
+  expect(userResult.email).toEqual(dto.email);
+  expect(userResult.username).toEqual(dto.username);
+  expect(userResult.locale).toEqual(dto.locale);
+  expect(userResult.provider).toEqual(dto.provider);
+
+  expect(firebaseUserResult.name).toEqual(dto.name);
+  expect(firebaseUserResult.email).toEqual(dto.email);
+  expect(firebaseUserResult.username).toEqual(dto.username);
+  expect(firebaseUserResult.locale).toEqual(dto.locale);
+  expect(firebaseUserResult.provider).toEqual(dto.provider);
+}
+
+function validateCreateResults(dto: any, userResult: any, firebaseUserResult?: any) {
+  expect(userResult).toBeTruthy();
+  expect(userResult?.id).toBeTruthy();
+  expect(userResult?.secrets?.id).toBeTruthy();
+  expect(userResult.id).toEqual(userResult?.secrets?.userId);
+
+  expect(firebaseUserResult).toBeTruthy();
+  expect(firebaseUserResult?.id).toBeTruthy();
+  expect(firebaseUserResult?.secrets?.id).toBeTruthy();
+  expect(firebaseUserResult.id).toEqual(userResult?.secrets?.userId);
+
+  validateUpdate(dto, userResult, firebaseUserResult);
+
+  expect(userResult?.secrets?.password).toEqual(dto?.secrets?.create.password);
+  expect(firebaseUserResult?.secrets?.password).toEqual(dto?.secrets?.create.password);
+}
+
 describe("IntegrationTesting of userService", () => {
   let moduleRef: TestingModule;
   let userService: UserService;
+  let firebaseUserService: FirebaseUserService;
 
   beforeAll(async () => {
     moduleRef = await Test.createTestingModule({
@@ -29,6 +62,7 @@ describe("IntegrationTesting of userService", () => {
     }).compile();
 
     userService = await moduleRef.resolve<UserService>(UserService);
+    firebaseUserService = await moduleRef.resolve<FirebaseUserService>(FirebaseUserService);
   });
 
   afterAll(async () => {
@@ -37,78 +71,83 @@ describe("IntegrationTesting of userService", () => {
 
   it("User service should be defined", () => {
     expect(userService).toBeDefined();
+    expect(firebaseUserService).toBeDefined();
   });
 
   describe("userService methods (create/delete)", () => {
     it("create (with email): User is created with secrets", async () => {
       const userDto = getFakeUserCreateBody({ provider: "email" });
       const result = await userService.create(userDto as any);
-
-      expect(result).toBeTruthy();
-      expect(result?.id).toBeTruthy();
-      expect(result?.secrets?.id).toBeTruthy();
-      expect(result.id).toEqual(result?.secrets?.userId);
-
-      expect(result.name).toEqual(userDto.name);
-      expect(result.email).toEqual(userDto.email);
-      expect(result.username).toEqual(userDto.username);
-      expect(result.locale).toEqual(userDto.locale);
-      expect(result.provider).toEqual(userDto.provider);
-      expect(result?.secrets?.password).toEqual(userDto?.secrets?.create.password);
+      const firebaseResult = await firebaseUserService.create(userDto as any);
+      
+      validateCreateResults(userDto, result, firebaseResult)
 
       await userService.deleteOneById(result.id);
+      await firebaseUserService.deleteOneById(user.id);
     });
 
     it("create (with google): User is created with secrets", async () => {
       const userDto = getFakeUserCreateBody({ provider: "google" });
       const result = await userService.create(userDto as any);
+      const firebaseResult = await firebaseUserService.create(userDto as any);
 
-      expect(result).toBeTruthy();
-      expect(result?.id).toBeTruthy();
-      expect(result?.secrets?.id).toBeTruthy();
-      expect(result.id).toEqual(result?.secrets?.userId);
-
-      expect(result.name).toEqual(userDto.name);
-      expect(result.email).toEqual(userDto.email);
-      expect(result.username).toEqual(userDto.username);
-      expect(result.locale).toEqual(userDto.locale);
-      expect(result.provider).toEqual(userDto.provider);
-      expect(result?.secrets?.password).toEqual(userDto?.secrets?.create.password);
+      validateCreateResults(userDto, result, firebaseResult)
 
       await userService.deleteOneById(result.id);
+      await firebaseUserService.deleteOneById(user.id);
     });
 
     it("deleteOneById: User is deleted", async () => {
       const userDto = getFakeUserCreateBody({ provider: "email" });
+
       const result = await userService.create(userDto as any);
+      const firebaseResult = await firebaseUserService.create(userDto as any);
 
       const response = await userService.deleteOneById(result.id);
+      const firebaseResponse = await firebaseUserService.deleteOneById(firebaseResult.id);
+
       expect(response).toBeTruthy();
       expect(response.id).toEqual(result.id);
+
+      expect(firebaseResponse).toBeTruthy();
+      expect(firebaseResponse.id).toEqual(result.id);
     });
   });
 
   describe("userService methods (read/edit)", () => {
     it("findOneById: User with secrets is found by its id", async () => {
       const userDto = getFakeUserCreateBody({ provider: "google" });
+
       const user = await userService.create(userDto as any);
+      const firebaseUser = await firebaseUserService.create(userDto as any);
+
       const id = user.id;
+      const firebaseId = firebaseUser.id;
 
       const result = await userService.findOneById(id);
+      const firebaseResult = await firebaseUserService.findOneById(firebaseId);
 
       expect(result).toEqual(user);
       expect(result?.secrets).toBeTruthy();
 
+      expect(firebaseResult).toEqual(user);
+      expect(firebaseResult?.secrets).toBeTruthy();
+
       await userService.deleteOneById(id);
+      await firebaseUserService.deleteOneById(user.id);
     });
   });
 
   it("updateByEmail: User is updated", async () => {
     const userDto = getFakeUserCreateBody({ provider: "google" });
-    const user = await userService.create(userDto as any);
-    const email = user.email;
 
-    const data = {
+    const user = await userService.create(userDto as any);
+    const firebaseUser = await firebaseUserService.create(userDto as any);
+    
+    const email = user.email;
+    const firebaseEmail = firebaseUser.email;
+
+    const updateData = {
       secrets: {
         update: {
           refreshToken: null,
@@ -116,15 +155,15 @@ describe("IntegrationTesting of userService", () => {
       },
     };
 
-    const result = await userService.updateByEmail(email, data);
+    const result = await userService.updateByEmail(email, updateData);
+    const firebaseResult = await firebaseUserService.updateByEmail(firebaseEmail, updateData);
 
-    expect(result.name).toEqual(user.name);
-    expect(result.email).toEqual(user.email);
-    expect(result.username).toEqual(user.username);
-    expect(result.locale).toEqual(user.locale);
-    expect(result.provider).toEqual(user.provider);
+    validateUpdate(user, result, firebaseUser);
+
     expect((result as any)?.secrets).toBeFalsy();
+    expect((firebaseResult as any)?.secrets).toBeFalsy();
 
     await userService.deleteOneById(user.id);
+    await firebaseUserService.deleteOneById(user.id);
   });
 });
