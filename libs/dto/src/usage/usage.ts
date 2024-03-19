aba66fcd453cbd82a4b1e6f6f5736431b69431b9
Julie Cherner <juliechernen@gmail.com>
2024-03-19 17:13:44 +0200

    Lib/Usage update dto type is updated

--- libs/dto/src/usage/usage.ts
+++ libs/dto/src/usage/usage.ts
@@ -4,10 +4,6 @@ import { z } from "nestjs-zod/z";
 
 const currentCount = z.object({ current: z.number().default(0) });
 
-const updateFiels = z.enum(["resumes", "downloads", "views", "alWords"]);
-
-export type UpdateFields = z.infer<typeof updateFiels>;
-
 export const usageSchema = z.object({
   id: z.string().optional(),
   userId: idSchema,
@@ -18,3 +14,14 @@ export const usageSchema = z.object({
 });
 
 export class UsageDto extends createZodDto(usageSchema) {}
+
+const updateFields = z.enum(["resumes", "downloads", "views", "alWords"]);
+export type UpdateFields = z.infer<typeof updateFields>;
+
+const actionTypes = z.enum(["increment", "decrement"]);
+export type ChangeFieldAction = z.infer<typeof actionTypes>;
+
+export type UpdateUsageDto = {
+  action: z.infer<typeof actionTypes>;
+  field: z.infer<typeof updateFields>;
+};
