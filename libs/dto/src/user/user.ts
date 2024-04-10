fe617387ecac5e9380bb34d4de755334ab7597a4
Julie Cherner <juliechernen@gmail.com>
2024-04-10 15:34:18 +0300

    Libs/Typo is corrected

--- libs/dto/src/user/user.ts
+++ libs/dto/src/user/user.ts
@@ -23,7 +23,7 @@ export const userSchema = z.object({
   emailVerified: z.boolean().default(false),
   twoFactorEnabled: z.boolean().default(false),
   provider: z.enum(["email", "github", "google"]).default("email"),
-  createdAt: z.date().or(z.dateString()).or(z.any()),
+  createdAt: z.date().or(z.dateString()),
   updatedAt: z.date().or(z.dateString()),
 });
 
