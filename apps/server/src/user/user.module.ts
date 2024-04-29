a273b7c262708a10ed92018299278f7882f26999
Julie Cherner <juliechernen@gmail.com>
2024-04-29 19:19:10 +0300

    Server/Service is extracted

--- apps/server/src/user/user.module.ts
+++ apps/server/src/user/user.module.ts
@@ -3,7 +3,6 @@ import { forwardRef, Module } from "@nestjs/common";
 import { AuthModule } from "../auth/auth.module";
 import { FirebaseModule } from "../firebase/firebase.module";
 import { UserController } from "./user.controller";
-import { PrismaUserService } from "./prisma-user.service";
 import { UserService } from "./user.service";
 import { UsageModule } from "../usage/usage.module";
 import { SubscriptionModule } from "../subscription/subscription.module";
@@ -11,7 +10,7 @@ import { SubscriptionModule } from "../subscription/subscription.module";
 @Module({
   imports: [forwardRef(() => AuthModule.register()), FirebaseModule, UsageModule, SubscriptionModule],
   controllers: [UserController],
-  providers: [PrismaUserService, UserService],
-  exports: [PrismaUserService, UserService],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
